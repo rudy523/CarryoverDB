83610867670c9840bddcc7b97f7961a95eba7162
James Rudy <rudy523@outlook.com>
2021-01-26 14:43:36 -0500

    first commit

---
+++ dbo.BO_Raw.data.sql
@@ -0,0 +1,9 @@
+﻿BULK INSERT RAW_DATA.dbo.BO_RAW
+    FROM 'C:\Users\550469\Documents\SCMS & IC2&I\Strategic Finance\BO Reports\Local_Navy_ERP_NAWC_Carry_Over 09-30-2020.csv'
+        WITH 
+        (
+            CHECK_CONSTRAINTS,
+            DATAFILETYPE = 'char',
+            ERRORFILE = 'C:\Projects\SCMS - IC2&I\SQL Server\CarryoverDB\error_log\data_load_error.csv',
+            
+        )
